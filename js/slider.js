7345219fee684b7d203fced1a73c9e19da02b04d
Subrat Mallick <m.subbrat@outlook.com>
2022-11-30 15:24:46 +0530

    aas

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+var sldid = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slidehead");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    sldid++;
+    if (sldid > x.length) { sldid = 1 }
+    x[sldid - 1].style.display = "block";
+    setTimeout(carousel, 4000);
+}
